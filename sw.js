698839231df89886928301c41e591f1b3cf65bd9
friesh-stack <friesh@gmx.de>
2026-03-08 21:23:06 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Blue Lagune Service Worker
+const CACHE = 'blue-lagune-v1';
+const ASSETS = [
+  '/blue-lagune-burano/',
+  '/blue-lagune-burano/index.html',
+  '/blue-lagune-burano/data.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
